5e59fc1baa5a1edd88dd902bd2b6331b8ff9f47d
Kostis Triantafyllakis <kostastriantaf@gmail.com>
2016-09-07 10:17:23 +0300

    Add eigenvalue detection block

---
+++ include/phasma/eigenvalue_signal_detector.h
@@ -0,0 +1,59 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_PHASMA_EIGENVALUE_SIGNAL_DETECTOR_H
+#define INCLUDED_PHASMA_EIGENVALUE_SIGNAL_DETECTOR_H
+
+#include <phasma/api.h>
+#include <gnuradio/sync_decimator.h>
+
+namespace gr
+{
+  namespace phasma
+  {
+
+    /*!
+     * \brief <+description of block+>
+     * \ingroup phasma
+     *
+     */
+    class PHASMA_API eigenvalue_signal_detector : virtual public gr::sync_decimator
+    {
+    public:
+      typedef boost::shared_ptr<eigenvalue_signal_detector> sptr;
+
+      /*!
+       * \brief Return a shared_ptr to a new instance of phasma::eigenvalue_signal_detector.
+       *
+       * To avoid accidental use of raw pointers, phasma::eigenvalue_signal_detector's
+       * constructor is in a private implementation
+       * class. phasma::eigenvalue_signal_detector::make is the public interface for
+       * creating new instances.
+       */
+      static sptr
+      make (size_t samples_num, size_t oversampling_factor,
+	    size_t smoothing_factor, double pfa, uint8_t algo);
+    };
+
+  } // namespace phasma
+} // namespace gr
+
+#endif /* INCLUDED_PHASMA_EIGENVALUE_SIGNAL_DETECTOR_H */
+
--- swig/phasma_swig.i
+++ swig/phasma_swig.i
@@ -9,8 +9,11 @@
 
 %{
 #include "phasma/rforest_model.h"
+#include "phasma/eigenvalue_signal_detector.h"
 %}
 
 
 %include "phasma/rforest_model.h"
 GR_SWIG_BLOCK_MAGIC2(phasma, rforest_model);
+%include "phasma/eigenvalue_signal_detector.h"
+GR_SWIG_BLOCK_MAGIC2(phasma, eigenvalue_signal_detector);
